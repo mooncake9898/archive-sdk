7a02c6b18332ea1b18de5b5dfa4a495a0461440b
jawaccomplishedclass <>
2024-05-22 15:11:27 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.js', 'dist/*'],
+  root: true,
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# Blueprint Lib（WIP）
+
+Library containing Blueprint Interface models and common logics.
+
+## How to install
+
+Add this package as a dependency to package.json
+
+## Changelog
+
+### Version 1.2.0
+- Moved `amountAdded` and `adjustmentValueUsd` properties from `PositionShares` class into `Operation` class
+
+publish this library as NPM package later on.
