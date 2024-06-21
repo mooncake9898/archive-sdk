0844d03a63b7acebc72b5086e49bb3ba96b81cee
jawaccomplishedclass <>
2024-06-21 16:19:18 +0900

    add metadata

--- dist/visionCache.js
+++ dist/visionCache.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
@@ -17,7 +20,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var VisionCache_1;
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VisionCache = void 0;
 /**
@@ -28,6 +35,8 @@ const apiCallResults_entity_1 = require("./apiCallResults.entity");
 // import { RequestContext } from './requestContext';
 const common_1 = require("@nestjs/common");
 const typeorm_1 = require("@nestjs/typeorm");
+const async_redis_1 = __importDefault(require("async-redis"));
+const typeorm_2 = require("typeorm");
 let VisionCache = VisionCache_1 = class VisionCache {
     constructor(cache, apiCallResultsRepo) {
         this.cache = cache;
@@ -139,6 +148,7 @@ VisionCache.PERM_CACHE_DURATION = 60 * 60 * 24 * 999;
 exports.VisionCache = VisionCache = VisionCache_1 = __decorate([
     (0, common_1.Injectable)(),
     __param(0, (0, common_1.Inject)('CACHE_ETH')),
-    __param(1, (0, typeorm_1.InjectRepository)(apiCallResults_entity_1.ApiCallResults))
+    __param(1, (0, typeorm_1.InjectRepository)(apiCallResults_entity_1.ApiCallResults)),
+    __metadata("design:paramtypes", [typeof (_a = typeof async_redis_1.default !== "undefined" && async_redis_1.default) === "function" ? _a : Object, typeorm_2.Repository])
 ], VisionCache);
 //# sourceMappingURL=visionCache.js.map
