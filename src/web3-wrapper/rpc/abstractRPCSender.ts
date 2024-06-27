d7d6b4997ee05c9f80f75a4efccb45d77c8cd0dc
mooncake <mooncake@gmail.com>
2024-06-27 14:43:29 +0700

    add should retry logic

--- src/web3-wrapper/rpc/abstractRPCSender.ts
+++ src/web3-wrapper/rpc/abstractRPCSender.ts
@@ -1,10 +1,13 @@
 export class AbstractRPCSender {
-
   protected getErrorMessage(error: any, rpcUrl: string): string {
     if (error.code === 'NETWORK_ERROR') {
       return `Error connecting to RPC ${rpcUrl}, message: ${error.message}`;
     } else {
       return `Error on RPC ${rpcUrl}, code: ${error.code}, message: ${error.message}`;
     }
+  }
+
+  protected shouldRetryError(error: any): boolean {
+    return ['NETWORK_ERROR', 'SERVER_ERROR'].includes(error.code) || [403, 429].includes(error.code);
   }
 }
