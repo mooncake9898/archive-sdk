1cd0d4481d5146c828dfb8521adb2ba0c0e8b940
mooncake9898 <mooncake9898@gmail.com>
2024-10-11 20:22:59 +0700

    update version

--- dist/blueprint/models/timeContext.js
+++ dist/blueprint/models/timeContext.js
@@ -5,6 +5,8 @@ class TimeContext {
     constructor(blockNumber, timestamp) {
         this.blockNumber = blockNumber;
         this.timestamp = timestamp;
+        this.blockNumber = Number(blockNumber);
+        this.timestamp = Number(timestamp);
     }
 }
 exports.TimeContext = TimeContext;
