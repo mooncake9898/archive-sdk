ef2daa4ab76c94f7fe1f4a2aa58a9cbd65e6f111
echohelmetenchanting <miguel@apy.vision>
2025-01-11 14:57:34 -0600

    revert removal of control characters from strings

--- dist/blueprint/models/classification.d.ts
+++ dist/blueprint/models/classification.d.ts
@@ -1,20 +1,13 @@
 import { Operation } from '../../blueprint/models/operation';
 import { PositionShares } from './positionShares';
 import BigNumber from 'bignumber.js';
-
 export declare class Classification {
-  operations: Operation[];
-  positionIdentifier: string;
-  gasTokenAmount: BigNumber;
-  positionShareDetails: PositionShares[];
-  userAddress: string;
-  constructor(
-    operations: Operation[],
-    positionIdentifier: string,
-    gasTokenAmount: BigNumber,
-    positionShareDetails: PositionShares[],
-    userAddress?: string,
-  );
-  getSharesAdded(): BigNumber;
-  getAdjustmentValueUsd(): BigNumber;
+    operations: Operation[];
+    positionIdentifier: string;
+    gasTokenAmount: BigNumber;
+    positionShareDetails: PositionShares[];
+    userAddress: string;
+    constructor(operations: Operation[], positionIdentifier: string, gasTokenAmount: BigNumber, positionShareDetails: PositionShares[], userAddress?: string);
+    getSharesAdded(): BigNumber;
+    getAdjustmentValueUsd(): BigNumber;
 }
